09e7a3fa1c5504d520f9ad912c87afc439a7d14d
Hifi58 <gillet.tony@gmail.com>
2021-05-25 11:44:17 +0200

    version finale

--- player.js
+++ player.js
@@ -2,37 +2,55 @@ const player = document.getElementById('player');
 const moveSize = 24;
 var playerWalk = 0;
 
+//déplacement vers le haut avec blocage
+
 document.addEventListener('keydown', function(event) {
     if (event.code == 'ArrowUp') {
-    
+    if(player.offsetTop > 0){
     playerWalk = playerWalk + 1;
     player.style.top = (player.offsetTop - moveSize) + "px";
+    
+//changement d'images un pas sur deux
+
     if(playerWalk % 2 == 0){
     player.style.backgroundImage = "url(img/up.png)";
-}else{
+    }else{
             player.style.backgroundImage = "url(img/wup.png)";
         };
-        
-    } else if (event.code == 'ArrowRight') {
+    };  
+
+//déplacement sur la droite avec blocage
+
+    }else if (event.code == 'ArrowRight') {
+        if(player.offsetLeft < 744){
     playerWalk = playerWalk + 1;
     player.style.left = (player.offsetLeft + moveSize) + "px";
+
     if(playerWalk % 2 == 0){
-        document.getElementById('player').style.backgroundImage = "url(img/right.png)";
+        player.style.backgroundImage = "url(img/right.png)";
     }else{
                 player.style.backgroundImage = "url(img/wright.png)";
             };
-    
-    } else if (event.code == 'ArrowDown') {
+        };
+
+//déplacement vers le bas avec blocage
+
+    }else if (event.code == 'ArrowDown') {
+     if(player.offsetTop < 744){  
     playerWalk = playerWalk + 1;
     player.style.top = (player.offsetTop + moveSize) + "px";
-
+    
     if(playerWalk % 2 == 0){
         player.style.backgroundImage = "url(img/face.png)";
     }else{
                 player.style.backgroundImage = "url(img/wface.png)";
             };
+ };
 
-    } else if (event.code == 'ArrowLeft') {
+//déplacement vers le bas déplacement sur la gauche avec blocage
+
+} else if (event.code == 'ArrowLeft') {
+        if(player.offsetLeft > 0){
     playerWalk = playerWalk + 1;
     player.style.left = (player.offsetLeft - moveSize) + "px";
     if(playerWalk % 2 == 0){
@@ -41,5 +59,6 @@ document.addEventListener('keydown', function(event) {
                 player.style.backgroundImage = "url(img/wleft.png)";
             };
     }
+   }
    });
    
